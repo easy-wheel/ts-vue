ff1a46169176a37abf8a12b385ddb7453fa1690d
Cosen95 <1263215592@qq.com>
2021-05-30 14:59:41 +0800

    fix 移除接口请求

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -18,21 +18,25 @@
         </template>
       </el-table-column>
       <el-table-column class-name="status-col" label="Status">
-        <template slot-scope="{row}">
-          <el-tag :type="row.status | articleStatusFilter">{{ row.status }}</el-tag>
+        <template slot-scope="{ row }">
+          <el-tag :type="row.status | articleStatusFilter">{{
+            row.status
+          }}</el-tag>
         </template>
       </el-table-column>
 
       <el-table-column align="center" label="Actions">
         <template slot-scope="scope">
-          <router-link :to="'/table/edit/'+scope.row.id">
-            <el-button type="primary" size="small" icon="el-icon-edit">Edit</el-button>
+          <router-link :to="'/table/edit/' + scope.row.id">
+            <el-button type="primary" size="small" icon="el-icon-edit"
+              >Edit</el-button
+            >
           </router-link>
         </template>
       </el-table-column>
     </el-table>
     <pagination
-      v-show="total>0"
+      v-show="total > 0"
       :total="total"
       :page.sync="listParams.page"
       :limit.sync="listParams.limit"
@@ -50,16 +54,16 @@ import Pagination from "@/components/Pagination/index.vue";
 @Component({
   name: "Table",
   components: {
-    Pagination
-  }
+    Pagination,
+  },
 })
 export default class extends Vue {
   private total = 0;
   private list: IArticleData[] = [];
-  private listLoading = true;
+  private listLoading = false;
   private listParams = {
     page: 1,
-    limit: 20
+    limit: 20,
   };
 
   created() {
@@ -67,15 +71,16 @@ export default class extends Vue {
   }
 
   private async getList() {
-    this.listLoading = true;
-    const { data } = await getArticles(this.listParams);
-    console.log("列表", data);
-    this.list = data.items;
-    this.total = data.total;
-    // Just to simulate the time of the request
-    setTimeout(() => {
-      this.listLoading = false;
-    }, 0.5 * 1000);
+    // fix: 移除接口请求
+    // this.listLoading = true;
+    // const { data } = await getArticles(this.listParams);
+    // console.log("列表", data);
+    // this.list = data.items;
+    // this.total = data.total;
+    // // Just to simulate the time of the request
+    // setTimeout(() => {
+    //   this.listLoading = false;
+    // }, 0.5 * 1000);
   }
 }
 </script>
